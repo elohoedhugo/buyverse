8bcd49ce4e4a2e59fd12a3bb558dc2b57c6d9430
elohoedhugo <elohoedhugo@gmail.com>
2025-05-25 18:13:21 +0100

    added footer

--- src/components/RootLayout.jsx
+++ src/components/RootLayout.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from './navbar/Navbar'
 import { Outlet } from 'react-router-dom'
+import Footer from './footer/Footer'
 
 const RootLayout = () => {
   return (
@@ -8,6 +9,7 @@ const RootLayout = () => {
     <header><Navbar/></header>
 
     <main><Outlet/></main>
+    <footer><Footer/></footer>
     </div>
   )
 }
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,16 +6,47 @@ const Footer = () => {
   return (
     <footer>
       <section>
-       <p>Get to know us</p>
+       <p className='footer-title'>Get to know us</p>
+       <div className='footer-content'>
+          <p>....About Us</p>
+          <p>....Careers</p>
+          <p>....Blogs</p>
+          <p>....Press Releases</p>
+          <p>....Investor Relations</p>
+       </div>
       </section>
       <section>
-        <p>Let us help you</p>
+        <p className='footer-title'>Let us help you</p>
+        <div className='footer-content'>
+          <p>....Your Account</p>
+          <p>....Returns Centre</p>
+          <p>....Help</p>
+          <p>....Shipping Rates</p>
+          <p>....Accessibility</p>
+       
+        </div>
       </section>
       <section>
-        <p>Payment & Security</p>
+        <p className='footer-title'>Payment & Security</p>
+        <div className='footer-content'>
+          <p>....Amazon Wallet</p>
+          <p>....Credit/Debit Cards</p>
+          <p>....Gift Cards</p>
+          <p>....EMI Payment Options</p>
+          <p>....Security</p>
+          
+        </div>
       </section>
       <section>
-        <p>Connect with us</p>
+        <p className='footer-title'>Connect with us</p>
+        <div className='footer-content'>
+          <p>....Facebook</p>
+          <p>....Twitter</p>
+          <p>....Instagram</p>
+          <p>....Youtube</p>
+          <p>....LinkedIn</p>
+          
+        </div>
       </section>
       </footer>
   )
