231ccce15ef9d2a83b331e19193b86b07defe2ae
elohoedhugo <elohoedhugo@gmail.com>
2025-05-25 17:40:05 +0100

    made extra changes on layout

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import "../footer/footer.css"
+
+
+const Footer = () => {
+  return (
+    <footer>
+      <section>
+       <p>Get to know us</p>
+      </section>
+      <section>
+        <p>Let us help you</p>
+      </section>
+      <section>
+        <p>Payment & Security</p>
+      </section>
+      <section>
+        <p>Connect with us</p>
+      </section>
+      </footer>
+  )
+}
+
+export default Footer
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import HeroSection from '../heroSection/HeroSection'
 import "../pages/home.css"
+import Footer from '../footer/Footer'
 
 
 const Home = () => {
   return (
     <div className='home'>
       <HeroSection/>
+      <Footer/>
       
     </div>
   )
